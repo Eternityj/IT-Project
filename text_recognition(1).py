a95351c174eb0b516e461d27e63271d8039dc436
yingtong-111 <116541724+yingtong-111@users.noreply.github.com>
2024-08-15 16:59:53 +1000

    Add files via upload
    This one for text_recognition

---
+++ text_recognition(1).py
@@ -0,0 +1,48 @@
+import cv2
+import pytesseract
+import time
+
+def capture_and_recognize_text():
+    # Capture a picture
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return ""
+
+    # Wait two seconds for the camera to stabilize before taking a picture
+    # -----------------------------
+    # Not using it for two seconds will result in a black screen
+    time.sleep(2)
+
+    # Discard the first few frames to ensure that you are capturing a stable image
+    # ----------------------
+    # can deletable
+    for i in range(5):
+        ret, frame = cap.read()
+
+    if not ret or frame is None:
+        print("Error: Could not capture image. Frame is empty.")
+        cap.release()
+        return ""
+
+    cap.release()
+
+    # Display image
+    # wait press key close window
+    cv2.imshow('Captured Image', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # Transformed gray scale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    # OCR text recognition using tesseract
+    text = pytesseract.image_to_string(gray)
+    return text
+
+if __name__ == "__main__":
+    recognized_text = capture_and_recognize_text()
+    print("text\n", recognized_text)
+
+
